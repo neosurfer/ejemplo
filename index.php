f16158a99f27668c01fff161931ee16359a00d90
Banny <solano.banny@gmail.com>
2020-03-26 22:44:18 -0500

    nuevo commitr

--- index.php
+++ index.php
@@ -1,12 +1,41 @@
 <html>
 <head>
 <title>
-::Welcome::
+::Clara::
 </title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
 </head>
 <body>
-<?php
-echo "hola mundo";
-?>
+<div class="row">
+<div class="col-md-12">
+<div class="media">
+    <div class="media-left media-middle">
+    <a href="#">
+        <img class="media-object" src="20191211_222245.jpg" alt="corazon" >
+    </a>
+    </div>
+    <div class="media-body">
+    <h4 class="media-heading">Antidoto y veneno</h4>
+    <p>Siempre intento de mi mente desprenderla
+Cada día más falta me hace, quiero verla
+Ya mis labios no soportan su ausencia
+Voy cayendo en un abismo por tenerla<p>
+
+<p>Y me amarra todo el vicio que ella esconde
+Al consumirla me esta consumiendo a mi
+Soy de ella más no es mía
+Es placer y es agonía, es antidoto y veneno</p>
+
+<p>
+Como droga que me mata a fuego lento
+Cuerpo y carne sin nada de sentimiento
+Quiere mi cuerpo y sólo eso sin amarlo
+Y yo queriéndole más todo momento
+</p>
+    </div>
+</div>
+
+</div>
+</div>
 </body>
 </html>
